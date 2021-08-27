9e9bb54bf6d7aee79ea595354f996ab37232972a
Jayanth Ayyadevara <jayanth@Jayanths-MacBook-Pro.local>
2021-08-27 19:05:09 +0530

    main commit

---
+++ main.js
@@ -0,0 +1,18 @@
+var d = new Date();
+
+var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+
+document.getElementById('wday').innerHTML = ("Happy " + days[d.getDay()] + "!<br><br>Please, get Vaccinated, and wear a mask. Save Lives.");
+
+
+
+const mouse = document.querySelector('.mouse');
+document.addEventListener('mousemove', e => {
+	let {clientX: x, clientY: y} = e;
+
+	mouse.style.transform = `translate(
+		${x - mouse.offsetWidth / 2}px,
+		${y - mouse.offsetHeight / 2}px
+	)`;
+	
+});
